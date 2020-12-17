310ca06dfcd48b0750193d9223dc76c47bdd3bee
cjy324 <juy32400@naver.com>
2020-12-17 18:43:17 +0900

    20.12.17 배포(JAVA게시판 생성, 게시글2 업로드)

---
+++ blog-status.sql
@@ -0,0 +1,32 @@
+## 사이트 운영 시작 ##
+
+#기존 사이트 게시글 및 사용자 초기화 
+TRUNCATE `article`;
+TRUNCATE `member`;
+TRUNCATE `board`;
+
+# 관리자 생성
+SELECT * FROM `member`;
+INSERT INTO `member`
+SET loginId = 'admin',
+loginPw = 'admin1!',
+`name` = 'Dev_J';
+
+
+# JAVA 관련 게시판 생성
+INSERT INTO `board`
+SET `name` = '공지사항',
+`code` = 'notice';
+
+INSERT INTO `board`
+SET `name` = '자유',
+`code` = 'free';
+
+INSERT INTO `board`
+SET `name` = 'JAVA',
+`code` = 'java';
+
+SELECT * FROM `article`;
+SELECT * FROM `member`;
+SELECT * FROM `board`;
+

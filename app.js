95fd0558e71103243137debf5c003400c68c8f46
cjy324 <juy32400@naver.com>
2020-12-22 19:25:23 +0900

    20.12.22 유튜브 영상 게시 기능 추가 및 5강 게시

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 
+/* 유튜브 함수 시작 */
 
-	function ArticleDetail__Body__init() {
-		if(toastui === undefined){
-			return;
-		}
-		
-		var body = document.querySelector('.article-detail-cell__body > div > span');
-		var initValue = body.innerHTML.trim();
-		
-		var viewer = new toastui.Editor.factory({
-			el : body,
-			initialValue : initValue,
-			viewer : true,
-			plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+	//유튜브 영상임을 감지하고 공간을 형성하는 함수
+	function youtubePlugin() {
+		toastui.Editor.codeBlockManager.setReplacer('youtube', youtubeId => {
+			// Indentify multiple code blocks
+			const wrapperId = `yt${Math.random()
+				.toString(36)
+				.substr(2, 10)}`;
+
+			// Avoid sanitizing iframe tag
+			setTimeout(renderYoutube.bind(null, wrapperId, youtubeId), 0);
+
+			return `<div id="${wrapperId}"></div>`;
 		});
-	};
+	}
+	//유튜브 영상 렌더링 함수
+	function renderYoutube(wrapperId, youtubeId) {
+		const el = document.querySelector(`#${wrapperId}`);
 
-	ArticleDetail__Body__init();
+		el.innerHTML = `<div class="toast-ui-youtube-plugin-wrap"><iframe src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+	}
+	/* 유튜브 함수 끝 */
+
+
+/* toastui-viewr 함수 시작 */
+
+function ArticleDetail__Body__init() {
+	if (toastui === undefined) {
+		return;
+	}
+
+	var body = document.querySelector('.article-detail-cell__body > div > span');
+	var initValue = body.innerHTML.trim();
+
+	
+	var viewer = new toastui.Editor.factory({
+		el: body,
+		initialValue: initValue,
+		viewer: true,
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight, youtubePlugin]
+	});
+};
+
+ArticleDetail__Body__init();
+
+/* toastui-viewr 함수 끝 */
